81b68d3b3c9c1fe82ce6ea348817c423cd3e0e68
noobyysauraj <gitsauraj@gmail.com>
2022-12-15 22:12:33 +0530

    setup: install simg2img

--- setup.sh
+++ setup.sh
@@ -18,14 +18,14 @@ if grep -q ^ID=alpine$ /etc/os-release; then
 	apk update -q --progress
 	apk add --progress --no-cache --purge --update-cache \
 		bash bash-completion binutils coreutils scanelf ncurses ncurses-dev findutils grep dpkg musl-dev libffi gawk sed file sharutils xterm \
-		wget curl aria2 ca-certificates gzip cpio bzip2 libbz2 lz4 xz-dev xz xz-libs zlib lzo lzop brotli tar zstd zstd-dev p7zip \
+		wget curl aria2 ca-certificates gzip cpio bzip2 libbz2 lz4 xz-dev xz xz-libs zlib simg2img lzo lzop brotli tar zstd zstd-dev p7zip \
 		gcc libgcc libstdc++ linux-headers libc-dev git libxml2 libfdt dtc-dev python3-dev openssh openssl libcrypto1.1 libssl1.1 gnupg detox xxd
 elif grep -q ^ID_LIKE=debian$ /etc/os-release; then
 	sed 's/main$/main universe/' /etc/apt/sources.list 1>/dev/null
 	apt-get update -qy
 	apt-get install -qy --show-progress coreutils build-essential gawk git-core sharutils uudeview mpack p7zip-full p7zip-rar \
 		gzip cpio bzip2 liblz4-dev liblz4-tool liblzma-dev xz-utils lzma lzop \
-		libxml2 libfdt-dev python3-dev aria2 detox brotli zstd libzstd-dev openssl xxd
+		libxml2 libfdt-dev python3-dev aria2 detox brotli zstd libzstd-dev openssl simg2img xxd
 fi
 
 if [ $? -eq 0 ]; then
